953db3f00fe662989818a0e85d27b6eba0e0a981
Sigrid Zänkert <sigrid.zaenkert@gmail.com>
2013-04-11 14:20:19 +0200

    Player werden zusammen mit Skeletons erzeugt

---
+++ wally/Player.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace wally
+{
+
+    public class Player
+    {
+        private ArrayList myPonylines;
+        private Polyline currentLine;
+        private Skeleton mySkel;
+        private int kinectProcessID;
+        private System.Windows.Point NewPoint;
+        private System.Windows.Media.Brush currentColor;
+        private double currentStroke;
+
+
+        public Player(int ID, Skeleton skel, Polyline line)
+        {
+            this.currentLine = line;
+            this.currentColor = System.Windows.Media.Brushes.White;
+            this.currentStroke = 5;
+            this.mySkel = skel;
+            this.kinectProcessID = ID;
+            this.myPonylines = new ArrayList();
+        }
+
+        public void addLine(Polyline line)
+        {
+            this.myPonylines.Add(line);
+            this.currentLine = line;
+        }
+        public Polyline getCurrentLine()
+        {
+            return this.currentLine;
+        }
+        public System.Windows.Media.Brush getCurrentColor()
+        {
+            return this.currentColor;
+        }
+        public double getCurrentStroke()
+        {
+            return this.currentStroke;
+        }
+        public System.Windows.Point getPoint()
+        {
+            return this.NewPoint;
+        }
+        public void setCurrentLine(Polyline line)
+        {
+            this.currentLine = line;
+            this.currentColor = line.Stroke;
+            this.currentStroke = line.StrokeThickness;
+        }
+        public void setCurrentColor(System.Windows.Media.Brush brush)
+        {
+            this.currentColor = brush;
+        }
+        public void setPoint(System.Windows.Point point)
+        {
+            this.NewPoint = point;
+        }
+        public void setCurrentStroke(double stroke)
+        {
+            this.currentStroke = stroke;
+        }
+        public Skeleton getSkeleton()
+        {
+            return this.mySkel;
+        }
+        public int getPlayersKinectId()
+        {
+            return this.kinectProcessID;
+        }
+        public ArrayList getPonyLines()
+        {
+            return this.myPonylines;
+        }
+        public int getPonyCount()
+        {
+            return this.myPonylines.Count;
+        }
+        public void addPointToCurrentLine(Point point)
+        {
+            this.currentLine.Points.Add(point); 
+        }
+
+    }
+}
